444703671ac4c7ed6035f8dbd99ca4520d678313
leobtt <leonarbat8@gmail.com>
2022-04-02 15:51:18 -0300

    react-virtualized

--- Components/SearchResults.tsx
+++ Components/SearchResults.tsx
@@ -1,20 +1,39 @@
 import { ResultsProps } from './interface'
 import { ProductItem } from './ProductItem'
-import { useMemo } from 'react'
+import {
+  List,
+  ListRowRenderer /* AutoSizer (para ocupar o tamanho da tela toda)*/,
+} from 'react-virtualized'
 
-export const SearchResult = ({ results, onAddToWishlist }: ResultsProps) => {
-  const totalPrice = useMemo(() => {
-    return results.reduce((acc, product) => {
-      return (acc += product.price)
-    }, 0)
-  }, [results])
+export const SearchResult = ({ results, totalPrice, onAddToWishlist }: ResultsProps) => {
+  const rowRender: ListRowRenderer = ({ index, key, style }) => {
+    return (
+      <div key={key} style={style}>
+        {' '}
+        {/* style controla basicamente se o elemento está visivel ou não em tela */}
+        <ProductItem product={results[index]} onAddToWishlist={onAddToWishlist} />
+      </div>
+    )
+  }
 
   return (
     <div>
-      {totalPrice}
-      {results.map((product) => {
+      <h2>{totalPrice}</h2>
+
+      <List
+        height={300} /* tamanho da nossa lista */
+        rowHeight={30} /* cada linha tem 30px de altura */
+        width={900}
+        overscanRowCount={
+          5
+        } /* significa que renderizará 5 itens a mais do que está demostrado na tela */
+        rowCount={results.length} /* quantos itens tem na lista */
+        rowRenderer={rowRender} /* uma função que renderizará a lista */
+      />
+
+      {/* {results.map((product) => {
         return <ProductItem key={product.id} product={product} onAddToWishlist={onAddToWishlist} />
-      })}
+      })} */}
     </div>
   )
 }
