3c6d2a6ec77ac05ad129051def23a1b08edbd9aa
leobtt <leonarbat8@gmail.com>
2022-04-02 15:49:56 -0300

    interface

--- Components/interface.ts
+++ Components/interface.ts
@@ -2,13 +2,20 @@ interface data {
   id: number
   price: number
   title: string
+  priceFormatted: string
 }
 
 export interface ResultsProps {
+  totalPrice: string
   results: data[]
   onAddToWishlist: (id: number) => void
 }
 export interface ProductProps {
   product: data
   onAddToWishlist: (id: number) => void
+}
+
+export interface AddProductToWishlistProps {
+  onAddToWishlist: () => void
+  onRequestClose: () => void
 }
